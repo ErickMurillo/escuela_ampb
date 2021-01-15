a6cf581d0725e1e3c0857e48c5cbf4c40a4e11c0
crocha <crocha09.09@gmail.com>
2021-01-15 11:05:41 -0600

    add config provider in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,32 @@
+import 'package:escuela_ampb/src/services/descarga_provider.dart';
 import 'package:flutter/material.dart';
 
 import 'package:escuela_ampb/src/pages/contenido_page.dart';
 import 'package:escuela_ampb/src/pages/home_page.dart';
 import 'package:escuela_ampb/src/pages/modulo_page.dart';
+import 'package:provider/provider.dart';
 
- 
+
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
 
     @override
     Widget build(BuildContext context) {
-        return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            title: 'Material App',
-            initialRoute: '/',
-            routes: {
-                '/' : (BuildContext context) => HomePage(),
-                'modulos' : (BuildContext context) => ModuloList(),
-                'contenido' : (BuildContext context) => ContenidoPage(),
-            },
+        return MultiProvider(
+          providers: [
+            ChangeNotifierProvider(create: ( _ ) => DescargaCurso() )
+          ],
+          child: MaterialApp(
+              debugShowCheckedModeBanner: false,
+              title: 'Material App',
+              initialRoute: '/',
+              routes: {
+                  '/' : (BuildContext context) => HomePage(),
+                  'modulos' : (BuildContext context) => ModuloList(),
+                  'contenido' : (BuildContext context) => ContenidoPage(),
+              },
+          ),
         );
     }
 }
