62707ba3eafd6b0318ab94efa0aad4020f55866b
crocha <crocha09.09@gmail.com>
2021-03-08 19:26:16 -0600

    add safe area

--- lib/src/pages/notas_page.dart
+++ lib/src/pages/notas_page.dart
@@ -8,51 +8,53 @@ class NotasPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: MyCustomAppBar(height: 120, texto: "Notas",search: false,),
-      body: Center(
-        child: Container(
-          child: Text("Notas pages"),
+    return SafeArea(
+      child: Scaffold(
+        appBar: MyCustomAppBar(height: 120, texto: "Notas",search: false,),
+        body: Center(
+          child: Container(
+            child: Text("Notas pages"),
+          ),
         ),
+        bottomNavigationBar: BottomNavigationBar(
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home',
+              ),
+              // BottomNavigationBarItem(
+              //   icon: Icon(Icons.search),
+              //   label: 'Buscar',
+              // ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.list),
+                label: 'Lista',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.book),
+                label: 'Notas',
+              ),
+            ],
+            currentIndex: _selectedIndex,
+            unselectedItemColor: Colors.black26,
+            selectedItemColor: Colors.amber[800],
+            showUnselectedLabels: false,
+            //backgroundColor: Colors.red,
+            onTap:(_selectedIndex){
+              switch(_selectedIndex){
+                case 0:
+                  Navigator.pushNamed(context, "/");
+                  break;
+                case 1:
+                  Navigator.pushNamed(context, "lista_cursos");
+                  break;
+                 case 2:
+                  Navigator.pushNamed(context, "notas");
+                  break;
+              }
+            },
+          ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
-            ),
-            // BottomNavigationBarItem(
-            //   icon: Icon(Icons.search),
-            //   label: 'Buscar',
-            // ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.list),
-              label: 'Lista',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.book),
-              label: 'Notas',
-            ),
-          ],
-          currentIndex: _selectedIndex,
-          unselectedItemColor: Colors.black26,
-          selectedItemColor: Colors.amber[800],
-          showUnselectedLabels: false,
-          //backgroundColor: Colors.red,
-          onTap:(_selectedIndex){
-            switch(_selectedIndex){
-              case 0:
-                Navigator.pushNamed(context, "/");
-                break;
-              case 1:
-                Navigator.pushNamed(context, "lista_cursos");
-                break;
-               case 2:
-                Navigator.pushNamed(context, "notas");
-                break;
-            }
-          },
-        ),
     );
   }
 }
